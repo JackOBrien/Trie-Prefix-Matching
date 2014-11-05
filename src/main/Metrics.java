47cf5ba9ca2dc09daba5ff27593dc67909c9fd0f
JohnOB2016@gmail.com <obrienjo@mail.gvsu.edu>
2014-11-05 17:28:47 -0500

    Added the .classpath and .project files back

--- src/main/Metrics.java
+++ src/main/Metrics.java
@@ -8,12 +8,23 @@ import java.util.Scanner;
 
 public class Metrics {
 	
+	static PrintStream dummyStream = new PrintStream(new OutputStream(){
+	    public void write(int b) {}
+	});
+	
+	static PrintStream originalStream;
+	
+	public static void mute() {
+		System.setOut(dummyStream);
+	}
+	
+	public static void unmute() {
+		System.setOut(originalStream);
+	}
+	
 	public static void main(String[] args) {
 		
-		PrintStream originalStream = System.out;
-		PrintStream dummyStream    = new PrintStream(new OutputStream(){
-		    public void write(int b) {}
-		});
+		originalStream = System.out;
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Number of times to populate the Trie: ");
@@ -32,47 +43,74 @@ public class Metrics {
 		
 		System.out.println("...Running...");
 		
-		System.setOut(dummyStream);
+		mute();
 		
-		long totalTime = 0;
+		long[] totalTime = {0l, 0l, 0l};
 		
-		for (int i = 0; i < numberOfLoops; i++) {
-			Router router = new Router();
+		for (int sL = 1; sL <= 3; sL ++) {
+			unmute();
+			System.out.println("Stride length " + sL);
+			mute();
 			
-			try {
-				router.setRoutesFile(routerFile);
-			} catch (FileNotFoundException e) {
-				System.err.println("File \"" + routerFile + "\" not found");
-				System.exit(0);
+			for (int i = 0; i < numberOfLoops; i++) {
+				Router router = new Router(sL);
+
+				try {
+					router.setRoutesFile(routerFile);
+				} catch (FileNotFoundException e) {
+					System.err.println("File \"" + routerFile + "\" not found");
+					System.exit(0);
+				}
+
+				try {
+					router.setIpListFile(ipFile);
+				} catch (FileNotFoundException e) {
+					System.err.println("File \"" + ipFile + "\" not found");
+					System.exit(0);
+				}
+
+				long start = System.currentTimeMillis();
+
+				try {
+					router.populateTrie();
+					router.lookupIPs();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				totalTime[sL-1] += (System.currentTimeMillis() - start);
+				unmute();
+				System.out.println("-- Run " + (i + 1) + " complete");
+				mute();
 			}
-			
-			try {
-				router.setIpListFile(ipFile);
-			} catch (FileNotFoundException e) {
-				System.err.println("File \"" + ipFile + "\" not found");
-				System.exit(0);
-			}
-			
-			long start = System.currentTimeMillis();
-			
-			try {
-				router.populateTrie();
-				router.lookupIPs();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			totalTime += (System.currentTimeMillis() - start);
+			unmute();
+			System.out.println();
+			mute();
 		}
 		
-		int averageTime = (int) (totalTime / numberOfLoops);
-		long totalMemory = (Runtime.getRuntime().totalMemory() - 
-				Runtime.getRuntime().freeMemory());
-		int averageMemory = (int) (totalMemory / numberOfLoops);
-		averageMemory /= 1048576;
+//		int averageTime = (int) (totalTime / numberOfLoops);
+//		long totalMemory = (Runtime.getRuntime().totalMemory() - 
+//				Runtime.getRuntime().freeMemory());
+//		int averageMemory = (int) (totalMemory / numberOfLoops);
+//		averageMemory /= 1048576;
 		
-		System.setOut(originalStream);
-		System.out.println("\nAverage time: " + averageTime + "ms");
-		System.out.println("Average memory: " + averageMemory + "Mb");
+		unmute();
+		
+		System.out.println("\n");
+		String header = String.format("%14s| %7s %7s %7s", "Stride Length",
+				"1", "2", "3");
+		System.out.println(header);
+		System.out.println(new String(new char[39]).replace("\0", "-"));
+		
+		String build = String.format("%14s| %7s %7s %7s", "Build Time",
+				"---", "---", "---");
+		String lookup = String.format("%14s| %7s %7s %7s", "Look Up Time",
+				"---", "---", "---");
+		String memory = String.format("%14s| %7s %7s %7s", "Memory Usage",
+				"---", "---", "---");
+		
+		System.out.println(build);
+		System.out.println(lookup);
+		System.out.println(memory);
 	}	
 }
